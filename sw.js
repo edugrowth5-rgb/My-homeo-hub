98924dbd7a20a308bdb1bbb32790c326cb2b877f
edugrowth5-rgb <edugrowth5@gmail.com>
2026-02-28 14:29:30 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'homeoguide-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
